f33d900a428f3860c5c459a729ded658a8589e39
zwk <2076637688@qq.com>
2024-04-14 20:45:59 +0800

    基本导出功能

--- src/plugins/ExportExcelButton.ts
+++ src/plugins/ExportExcelButton.ts
@@ -4,6 +4,53 @@ import { FolderSingle } from '@univerjs/icons';
 import * as UniverJS from "@univerjs/core";
 import * as ExcelJS from 'exceljs';
 
+//格式化时间
+const formatDate = (date: Date) => {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+}
+
+//下载文件
+const download_file = (buffer: ExcelJS.Buffer, fileName: string) => {
+  let fileURL = window.URL.createObjectURL(new Blob([buffer]));
+  let fileLink = document.createElement("a");
+  fileLink.href = fileURL;
+  fileLink.setAttribute("download", fileName);
+  document.body.appendChild(fileLink);
+  fileLink.click();
+}
+
+//下面是导出的函数
+const excelExport = async (univerWorkbook: UniverJS.Workbook) => {
+  const workbook = new ExcelJS.Workbook();
+  const sheetMap = univerWorkbook.getWorksheets()
+  sheetMap.forEach(sheet => {
+    const worksheet = workbook.addWorksheet(sheet.getName());
+    // 遍历行
+    for (let row = 0; row < sheet.getRowCount(); row++) {
+      const rowData: any[] = [];
+      // 遍历列
+      for (let col = 0; col < sheet.getColumnCount(); col++) {
+        const cell = sheet.getCell(row, col);
+        if (cell) {
+          rowData[col] = cell.v
+        }
+      }
+      worksheet.addRow(rowData);
+    }
+  })
+
+  // 写入文件
+  const buffer = await workbook.xlsx.writeBuffer();
+  //下载文件
+  download_file(buffer, formatDate(new Date()) + "导出excel.xlsx");
+}
+
 /**
  * Export Excel Button Plugin
  * A simple Plugin example, show how to write a plugin.
@@ -44,19 +91,7 @@ class ExportExcelButtonPlugin extends UniverJS.Plugin {
         // inject univer instance service
         const univer = accessor.get(UniverJS.IUniverInstanceService);
         const univerWorkbook = univer.getCurrentUniverSheetInstance()
-        const sheetMap = univerWorkbook.getWorksheets()
-        sheetMap.forEach(sheet => {
-          // 遍历行
-          for (let row = 0; row < sheet.getRowCount(); row++) {
-            // 遍历列
-            for (let col = 0; col < sheet.getColumnCount(); col++) {
-              const cell = sheet.getCell(row, col);
-              if (cell) {
-                const cellAddress = String.fromCharCode('A'.charCodeAt(0) + col) + (row + 1);
-              }
-            }
-          }
-        })
+        excelExport(univerWorkbook);
       },
     };
     this.commandService.registerCommand(command);
